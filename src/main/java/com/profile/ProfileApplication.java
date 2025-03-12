52eac44cd77e5caac3d10708d9557dea1d6eb1d8
Ariel Wilkomm <ariel.wilkomm@compasso.com.br>
2025-03-12 15:23:10 -0300

    initial

--- src/main/java/com/profile/ProfileApplication.java
+++ src/main/java/com/profile/ProfileApplication.java
@@ -1,13 +1,33 @@
 package com.profile;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@Configuration
+@Component
+@Slf4j
+@EnableCaching
+@EnableFeignClients
 public class ProfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+		log.info("Start LocalDateTime: " + LocalDateTime.now());
 	}
 
 }
