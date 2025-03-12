512bcc93b7fff5a4096015932c8b132a733f4330
Ariel Wilkomm <ariel.wilkomm@compasso.com.br>
2025-03-12 17:31:02 -0300

    initial

--- src/main/java/com/profile/controller/ProfileController.java
+++ src/main/java/com/profile/controller/ProfileController.java
@@ -3,15 +3,18 @@ package com.profile.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import records.exceptions.ErrorResponseRecords;
 import records.profile.ProfileRecord;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 @RestController
@@ -45,7 +48,7 @@ public class ProfileController {
     )
     public ProfileRecord getProfile(@PathVariable("cpf") String cpf) {
         log.info("Getting profile for customer: " + cpf);
-        return new ProfileRecord("12345678900", "John Doe", "John", "Doe", "john.doe@email.com", "55912345678");
+        return new ProfileRecord("12345678900", "John Doe", "john.doe@email.com", "55912345678");
     }
 
     @PostMapping(value = "/profile", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -53,14 +56,6 @@ public class ProfileController {
             summary = "Create profile",
             description = "Create a new profile information",
             security = @SecurityRequirement(name = "PassThrough"),
-            requestBody = @RequestBody(
-                    description = "Profile data to be created",
-                    required = true,
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = ProfileRecord.class)
-                    )
-            ),
             responses = {
                     @ApiResponse(
                             responseCode = "201",
@@ -80,8 +75,25 @@ public class ProfileController {
                     )
             }
     )
-    public ProfileRecord createProfile(@RequestBody ProfileRecord profileRecord) {
-        log.info("Creating profile for customer: " + profileRecord.cpf());
-        return profileRecord;
+    public ProfileRecord createProfile(@RequestBody @Valid ProfileRecord profileRecord) {
+        log.info("Creating profile for profile: " + profileRecord.cpf());
+
+        return new ProfileRecord(
+                profileRecord.cpf(),
+                profileRecord.name(),
+                profileRecord.email(),
+                profileRecord.phone()
+        );
+    }
+
+    private String[] extractFirstAndLastName(String name) {
+        Pattern pattern = Pattern.compile("^([\\w\\-]+)\\s([\\w\\-]+)$");
+        Matcher matcher = pattern.matcher(name);
+
+        if (matcher.matches()) {
+            return new String[]{matcher.group(1), matcher.group(2)};
+        } else {
+            return new String[]{name, ""};
+        }
     }
 }
--- src/main/java/records/profile/ProfileRecord.java
+++ src/main/java/records/profile/ProfileRecord.java
@@ -1,27 +1,26 @@
 package records.profile;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 @Builder
 public record ProfileRecord(
 
         @Schema(description = "User's CPF", example = "12345678900")
+        @NotEmpty(message = "CPF cannot be empty")
         String cpf,
 
         @Schema(description = "User's full name", example = "John Doe")
+        @NotEmpty(message = "Name cannot be empty")
         String name,
 
-        @Schema(description = "User's first name", example = "John")
-        String firstName,
-
-        @Schema(description = "User's last name", example = "Doe")
-        String lastName,
-
         @Schema(description = "User's email address", example = "john.doe@email.com")
+        @NotEmpty(message = "Email cannot be empty")
         String email,
 
         @Schema(description = "User's phone number", example = "55912345678")
+        @NotEmpty(message = "Phone cannot be empty")
         String phone
 
 ) { }
